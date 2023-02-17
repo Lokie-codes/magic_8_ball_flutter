29367f43ed7ba98e70f47f0fc48f441efee617da
Lokesh S <lokeshlucky13579@gmail.com>
2023-02-17 16:20:40 +0530

    initial commit Scaffold project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ask me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        backgroundColor: Colors.blue,
+        body: Container(
+          
+        ),
+      ),
+    ),
+  );
+}
+
