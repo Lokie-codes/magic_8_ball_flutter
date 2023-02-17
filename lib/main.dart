aa2e8c97752bfb765edf107648d1ebcaa2f00308
Lokesh S <lokeshlucky13579@gmail.com>
2023-02-17 16:40:39 +0530

    adding button and randomizing the event

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,11 +11,60 @@ void main() {
           backgroundColor: Colors.blue.shade900,
         ),
         backgroundColor: Colors.blue,
-        body: Container(
-          
-        ),
+        body: const Ball(),
       ),
     ),
   );
 }
 
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void changeNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: TextButton(
+            onPressed: () {
+              changeNumber();
+            },
+            child: Image.asset(
+              'images/ball$ballNumber.png',
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            changeNumber();
+          },
+          style: ElevatedButton.styleFrom(
+            textStyle: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.blue,
+          ),
+          child: const Text(
+            'Ask Now!',
+          ),
+        ),
+      ],
+    );
+  }
+}
